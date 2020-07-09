d15c52ca17fddbf0208041bf0b7f9dad6b893957
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-08 22:30:06 -0300

    configura a criação das tabelas ao inciar a aplicação

--- biblioteca/src/main/java/br/com/codenation/BibliotecaApplication.java
+++ biblioteca/src/main/java/br/com/codenation/BibliotecaApplication.java
@@ -2,8 +2,10 @@ package br.com.codenation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
